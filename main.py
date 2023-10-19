40c7c7adb5c5199368398f17fdd4fb2c7b03e116
Myrto Gk <myrtogk12@gmail.com>
2023-10-19 22:24:17 +0300

    cleaning data

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Vouleutis GPT")
+import pandas as pd
+
+df = pd.read_csv("YOUR_CSV_PATH")
+
+# Dropping all the rows where the column `member_name` is null, because these speeches don't provide any useful information
+df = df.dropna(subset=['member_name'])
+
