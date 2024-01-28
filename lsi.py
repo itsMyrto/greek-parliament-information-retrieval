8403a0273ad0850e910dd44a7f5e8d2ec55d416c
Myrto Gk <myrtogk12@gmail.com>
2024-01-28 23:15:20 +0200

    Added comments to LSI (part4 and part5)

--- lsi.py
+++ lsi.py
@@ -18,30 +18,37 @@ CLUSTERS = 500
 CLUSTER_ID = 100
 THRESHOLD = 80
 
+""" This function constructs the matrix where the LSI will be performed 
+    :returns LSI matrix, the term matrix and the document matrix
+"""
 def construct_matrix() -> np.array:
 
-    # We load the inverse index catalogue
+    """ We load the inverse index catalogue """
     if not os.path.isfile("inverse_index.pkl"):
         create_inverse_index_catalogue()
 
     with open("inverse_index.pkl", 'rb') as file:
         inverse_index_catalogue = pickle.load(file)
 
-    # A 2D empty row-array  is created in order to store the different terms
+    """ A 2D empty row-array  is created in order to store the different terms """
     terms = np.empty((1, len(inverse_index_catalogue)), dtype=object)
 
-    # A 2D empty column-array is created in order to store the document id's
+    """ A 2D empty column-array is created in order to store the document id's """
     documents = np.empty((NUMBER_OF_DOCS, 1), dtype=object)
 
-    # A 2D array full of zeros is created in order to store 1's and 0's
-    # 1 if a term exists in a document, 0 otherwise
-    # In this matrix the SVD will be applied
+    """
+    A 2D array full of zeros is created in order to store 1's and 0's
+    1 if a term exists in a document, 0 otherwise
+    In this matrix the SVD will be applied 
+    """
     matrix = np.zeros((NUMBER_OF_DOCS, len(inverse_index_catalogue)), np.float32)
 
-    # This block loops through the inverse index catalogue and for
-    # each word it finds which documents ids contain the specific word
-    # in order to update the matrix array. It also adds each term in the
-    # term array and each document id in the documents array
+    """
+    This block loops through the inverse index catalogue and for
+    each word it finds which documents ids contain the specific word
+    in order to update the matrix array. It also adds each term in the
+    term array and each document id in the documents array
+    """
     term_counter = 0
     for term, term_list in inverse_index_catalogue.items():
         terms[0][term_counter] = term
@@ -53,11 +60,13 @@ def construct_matrix() -> np.array:
 
     return matrix, terms, documents
 
-
+"""
+This function performs the LSI algorithm AND also creates clusters of speeches with common subject
+"""
 def LSI() -> np.array:
     matrix, terms, documents = construct_matrix()
 
-    # This function returns the top k singular values of the decomposition
+    """ This function returns the top k singular values of the decomposition """
     U, S, Vh = svds(matrix, k=THRESHOLD)
 
     print("Arrays from LSI")
@@ -65,10 +74,12 @@ def LSI() -> np.array:
     print(S.shape, S)
     print(Vh.shape, Vh)
 
-    # This is a 2D matrix that contains the document representation in a multidimensional space
-    # The representation used is the term to concept
-    # The formula for this representation is: document_concept = document * V
-    # projecting the original data into the space defined by the top singular vectors
+    """
+    This is a 2D matrix that contains the document representation in a multidimensional space
+    The representation used is the term to concept
+    The formula for this representation is: document_concept = document * V
+    projecting the original data into the space defined by the top singular vectors
+    """
     documents_representation = np.matmul(matrix, np.transpose(Vh))
 
     print("Document representation in multi-dimensional space")
@@ -76,7 +87,7 @@ def LSI() -> np.array:
 
     kmeans = KMeans(n_clusters=CLUSTERS, random_state=42, n_init="auto")
 
-    # Applying the kmeans algorithm and then save for every point the cluster id it belongs in a new column in the dataframe
+    """ Applying the kmeans algorithm and then save for every point the cluster id it belongs in a new column in the dataframe """
     cluster_id = kmeans.fit_predict(documents_representation)
 
     df_ = pd.read_csv(FILEPATH)
